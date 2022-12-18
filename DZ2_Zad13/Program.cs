ada2e979fca1f9986c709a7187cfe6311a6888a5
Andrey <rf5800@mail.ru>
2022-12-18 13:48:55 +0300

    DZ2_Zad13

---
+++ DZ2_Zad13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет
+
+Console.WriteLine("Введите число, которое является не менее чем двухзначным");
+int numbers = Convert.ToInt32(Console.ReadLine());
+string num = Math.Abs(numbers).ToString();
+if (num.Length > 2)
+{
+Console.Write("Третьей цифрой является: ");  
+Console.Write(num[2]);
+}
+else 
+{
+Console.Write("Третьей цифы нет!");
+}
